e212d1234e4acb82f5c065615a5c5c3d01b0f8f1
hannafachlany <hannafachlany04@gmail.com>
2025-03-15 14:46:19 +0700

    weeeeee

--- src/main/kotlin/com/example/kidsnesia/model/produkResponse.kt
+++ src/main/kotlin/com/example/kidsnesia/model/produkResponse.kt
@@ -3,8 +3,12 @@ package com.example.kidsnesia.model
 data class ProdukResponse(
     val idProduk: Long,
     val namaProduk: String,
-    val fotoProduk: String
-)
+    private val fotoProduk: String // Gunakan variabel privat untuk menyimpan path asli
+) {
+    val FotoProduk: String
+        get() = "http://192.168.1.57:8080/$fotoProduk" // Otomatis menambahkan prefix URL
+}
+
 
 // 🔥 Wrapper untuk daftar produk
 data class ProdukListWrapper(
